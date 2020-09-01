2defc3d99c22916c2e44ce3df8d4ac73320dbde5
lastrider <62941695+lastrider@users.noreply.github.com>
2020-09-01 18:42:33 +0200

    next commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://app-leonowicz-tasks.herokuapp.com/v1/task';
+  var apiRoot = 'https://app-leonowicz-tasks.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
